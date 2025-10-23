a8bd14f278f1973a0872c8dc44f1fcefd42c5830
esercitazione_25/26 <alessandrorellini17@gmail.com>
2025-10-23 12:41:00 +0200

    commit -m

--- src/GiocaNumeri.java
+++ src/GiocaNumeri.java
@@ -5,13 +5,13 @@ public class GiocaNumeri {
         Giocatore g1 = new Giocatore("Mario", estrattore);
         Giocatore g2 = new Giocatore("Bros", estrattore);
 
-        g1.run();
-        g2.run();
+        g1.run(); // avvio del thread per il giocatore 1
+        g2.run();  // avvio del thread per il giocatore 2
 
 
         try {
-            g1.join();
-            g2.join();
+            g1.join();   // aspetta prima di proseguire e sincronizza
+            g2.join();   // aspetta prima di proseguire e sincronizza
         } catch (InterruptedException e) {
             System.out.println("errore");
         }
